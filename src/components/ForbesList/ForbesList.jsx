77622227bd5f9349c8521b36a7b031ca709d4afa
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2024-01-11 15:27:12 +0200

    task3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,27 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import { ForbesListItem } from 'components/ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+
+export const ForbesList = ({ list }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map(({ id, name, avatar, capital, isIncrease }) => (
+          <ForbesListItem
+            key={id}
+            name={name}
+            avatar={avatar}
+            capital={capital}
+            isIncrease={isIncrease}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
